9dbe70a6bff0b899d703824bb02a0bbdc228d3b0
Ernesto Gigliotti <ernestogigliotti@gmail.com>
2021-08-26 21:45:55 -0300

    limpie los comentarios, ahora funciona

--- src/Clase2_2c.c
+++ src/Clase2_2c.c
@@ -23,40 +23,18 @@ int main(void)
 
 	// TIPO NOMBRE ;
 	int a=0; // numeros enteros
-	float f; // numeros con decimales
-	char c;  // 1 caracter = 1 letra = 1 simbolo.
-
-	printf("Ingresame tu eda':");
 
 	LIMPIAR_BUFFER_ENTRADA(); // limpiar el buffer de entrada antes de leer
-	scanf("%d",&a); // leemos
-	printf("Ingresaste la edad:%d",a);
-
-	// bUCLES
-	// while
-	// for
-	// do while
-
-	// CUALQUIERAAAA SE PUEDE USAR PARA CUALQUIER COSA
-	// SON EQUIVALENTES
-
-	// generalemte:
-	// si se cuantas veces voy a iterar -> for
-	// si no se -> while
-	// primero quiero iterar y despues preguntar por la condicion -> do while
 
 	char respuesta;
 
 	do
 	{
-		// slfhksdjjfksdkjh
 		printf("Continuar?s-n");
 		fflush(stdin);
 		scanf("%c",&respuesta);
 	}
 	while(respuesta=='s');
-
-
 
 	return EXIT_SUCCESS;
 }
